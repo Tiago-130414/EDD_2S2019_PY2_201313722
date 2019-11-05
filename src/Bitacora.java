afe2785c4e75306f276bb987adad3f8068e7940c
Santiago Rivadeneira <santiriva1994@gmail.com>
2019-11-05 01:12:38 -0600

    Se realizaron las principales funciones de la bitacora

--- src/Bitacora.java
+++ src/Bitacora.java
@@ -3,5 +3,46 @@
  * @author santi
  */
 public class Bitacora {
-    
+
+    NodoPila top;
+
+    public Bitacora() {
+        top = null;
+    }
+
+    public boolean estaVacia() {
+        return top == null;
+    }
+
+    public void insertarBitacora(String fech, String Hor, String Usu) {
+        NodoPila nuevo = new NodoPila(fech, Hor, Usu);
+        if (estaVacia()) {
+            top = nuevo;
+            System.out.println("insertado al inicio");
+        } else {
+            nuevo.siguiente = top;
+            top = nuevo;
+            System.out.println("insertado en pila");
+        }
+    }
+
+    public void mostrarBitacora() {
+        NodoPila aux = top;
+        if (estaVacia()) {
+            System.out.println("Bitacora Vacia");
+        } else {    
+            while (aux != null) {
+                System.out.println(aux.UsuarioHCambio);
+                aux = aux.siguiente;
+            }
+        }
+    }
+
+    public void eliminarBitacora() {    
+        if(estaVacia()){
+            System.out.println("Bitacora vacia no hay elementos para limpiar");
+        }else{
+            top = top.siguiente;
+        }
+    }
 }
